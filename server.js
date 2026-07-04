c7b86c7fc7b6cbfe3907181eab0071ffd13be875
ych19860118-hash <ych19860118@gmail.com>
2026-07-04 09:54:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+')(http);
 
 app.use(express.static(__dirname));
 
-let shapes = {}; // 儲存地圖物件資料
+// 儲存地圖物件資料結構：{ [id]: shapeObject }
+let shapes = {}; 
 
 io.on('connection', (socket) => {
   let userRoom = '';
 
+  // 使用者加入房間
   socket.on('join', (data) => {
     userRoom = data.room;
     socket.join(userRoom);
@@ -17,22 +16,35 @@ io.on('connection', (socket) => {
     socket.emit('init-shapes', Object.values(shapes).filter(s => s.room === userRoom));
   });
 
+  // 新增圖形
   socket.on('shape:add', (shape) => {
     shape.room = userRoom;
     shapes[shape.id] = shape;
     io.to(userRoom).emit('shape:added', shape);
   });
 
+  // 更新圖形（拖曳、調整大小、修改屬性）
   socket.on('shape:update', (updatedShape) => {
     if (shapes[updatedShape.id]) {
-      shapes[updatedShape.id] = { ...shapes[updatedShape.id], ...updatedShape };
+      // 合併新舊資料，確保屬性、類型與房間資訊完整
+      shapes[updatedShape.id] = { 
+        ...shapes[updatedShape.id], 
+        ...updatedShape,
+        properties: {
+          ...shapes[updatedShape.id].properties,
+          ...updatedShape.properties
+        }
+      };
       io.to(userRoom).emit('shape:updated', shapes[updatedShape.id]);
     }
   });
 
+  // 刪除圖形
   socket.on('shape:remove', (id) => {
-    delete shapes[id];
-    io.to(userRoom).emit('shape:removed', id);
+    if (shapes[id]) {
+      delete shapes[id];
+      io.to(userRoom).emit('shape:removed', id);
+    }
   });
 });
 
